c74c2ad0b14c006dcf741a86f31de8ec876e2469
unknown <vzeledon7@gmail.com>
2020-12-13 16:52:52 -0600

    Comenzamos

---
+++ app/src/main/java/com/poxs44/miproyectoandroidcompartido/Inicio.kt
@@ -0,0 +1,11 @@
+package com.poxs44.miproyectoandroidcompartido
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class Inicio : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_inicio)
+    }
+}
